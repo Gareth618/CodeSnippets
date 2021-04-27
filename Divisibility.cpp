b41baf07dd458cb2439bf83ccb757418bb868cc8
Gareth618 <bloggareth@gmail.com>
2021-04-27 13:20:59 +0300

    Added some number theory

---
+++ Divisibility.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+bool prime(T n) {
+    if (n == 1)
+        return false;
+    if (n % 2 == 0)
+        return n == 2;
+    for (T d = 3; d * d <= n; d += 2)
+        if (n % d == 0)
+            return false;
+    return true;
+}
+
+template<class T>
+vector<pair<T, int>> getDiv(T n) {
+    vector<pair<T, int>> div;
+    auto divide = [&](T d) {
+        if (n % d)
+            return;
+        div.emplace_back(d, 0);
+        while (n % d == 0) {
+            div.back().second++;
+            n /= d;
+        }
+    };
+    divide(2);
+    for (T d = 3; d * d <= n; d += 2)
+        divide(d);
+    if (n > 1)
+        div.emplace_back(n, 1);
+    return div;
+}
+
+template<class T>
+int divNum(const vector<pair<T, int>>& div) {
+    int ans = 1;
+    for (auto [d, e] : div)
+        ans *= e + 1;
+    return ans;
+}
+
+template<class T>
+T divSum(const vector<pair<T, int>>& div) {
+    T ans = 1;
+    for (auto [d, e] : div) {
+        T pwr = d;
+        for (int i = 0; i < e; i++)
+            pwr *= d;
+        ans *= (pwr - 1) / (d - 1);
+    }
+    return ans;
+}
+
+template<class T>
+T phi(const vector<pair<T, int>>& div) {
+    T ans = 1;
+    for (auto [d, e] : div) {
+        ans *= d - 1;
+        for (int i = 1; i < e; i++)
+            ans *= d;
+    }
+    return ans;
+}
+
+vector<int> phiUpToN(int n) {
+    vector<int> phi(n + 1);
+    for (int i = 1; i <= n; i++)
+        phi[i] = i;
+    for (int i = 1; i <= n; i++)
+        for (int j = 2 * i; j <= n; j += i)
+            phi[j] -= phi[i];
+    return phi;
+}
---
+++ Sieve.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Sieve {
+    vector<bool> sieve;
+    vector<int> primes;
+
+public:
+    Sieve(int n) : sieve(n + 1) {
+        sieve[0] = sieve[1] = true;
+        for (int j = 4; j <= n; j += 2)
+            sieve[j] = true;
+        for (int i = 3; i * i <= n; i += 2)
+            if (!sieve[i])
+                for (int j = i * i; j <= n; j += 2 * i)
+                    sieve[j] = true;
+        primes.push_back(2);
+        for (int i = 3; i <= n; i += 2)
+            if (!sieve[i])
+                primes.push_back(i);
+    }
+
+    auto begin() { return primes.begin(); }
+    auto end() { return primes.end(); }
+
+    template<class T>
+    bool prime(T n) {
+        if (n < (int) sieve.size())
+            return !sieve[n];
+        for (int d : primes) {
+            if (1LL * d * d > n)
+                break;
+            if (n % d == 0)
+                return false;
+        }
+        return true;
+    }
+
+    template<class T>
+    vector<pair<T, int>> getDiv(T n) {
+        vector<pair<T, int>> div;
+        auto divide = [&](int d) {
+            if (n % d)
+                return;
+            div.emplace_back(d, 0);
+            while (n % d == 0) {
+                div.back().second++;
+                n /= d;
+            }
+        };
+        for (int d : primes) {
+            if (1LL * d * d > n)
+                break;
+            divide(d);
+        }
+        if (n > 1)
+            div.emplace_back(n, 1);
+        return div;
+    }
+};
