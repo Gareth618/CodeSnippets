d646fb4ec6f798e56a624daefe9e568011dab560
Gareth618 <bloggareth@gmail.com>
2021-10-28 17:48:59 +0300

    Removed N from Matrix template

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,54 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<int N, int MOD = int(1e9 + 7)>
-class Vector {
-    int vec[N];
+template<int MOD = int(1e9 + 7)>
+struct Vector {
+    int n;
+    vector<int> vec;
 
-public:
-    Vector() : vec() { }
-    inline int& operator[](int pos) { return vec[pos]; }
+    Vector(int n) : n(n), vec(n) { }
+    int& operator[](int pos) { return vec[pos]; }
 };
 
-template<int N, int MOD = int(1e9 + 7)>
-class Matrix {
-    int mat[N][N];
+template<int MOD = int(1e9 + 7)>
+struct Matrix {
+    int n;
+    vector<vector<int>> mat;
 
-public:
-    Matrix() : mat() { }
-    inline int* operator[](int row) { return mat[row]; }
+    Matrix(int n) : n(n), mat(n, vector<int>(n)) { }
+    vector<int>& operator[](int row) { return mat[row]; }
 
-    static Matrix null() {
-        Matrix<N, MOD> mat;
-        for (int i = 0; i < N; i++)
+    static Matrix null(int n) {
+        Matrix<MOD> mat(n);
+        for (int i = 0; i < n; i++)
             mat[i][i] = 1;
         return mat;
     }
 };
 
-template<int N, int MOD>
-Vector<N, MOD> operator*(Matrix<N, MOD> a, Vector<N, MOD> b) {
-    Vector<N, MOD> c;
-    for (int i = 0; i < N; i++)
-        for (int k = 0; k < N; k++)
+template<int MOD>
+Vector<MOD> operator*(Matrix<MOD> a, Vector<MOD> b) {
+    if (a.n != b.n) throw exception();
+    const int n = a.n;
+    Vector<MOD> c;
+    for (int i = 0; i < n; i++)
+        for (int k = 0; k < n; k++)
             c[i] = (c[i] + 1LL * a[i][k] * b[k]) % MOD;
     return c;
 }
 
-template<int N, int MOD>
-Matrix<N, MOD> operator*(Matrix<N, MOD> a, Matrix<N, MOD> b) {
-    Matrix<N, MOD> c;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            for (int k = 0; k < N; k++)
+template<int MOD>
+Matrix<MOD> operator*(Matrix<MOD> a, Matrix<MOD> b) {
+    if (a.n != b.n) throw exception();
+    const int n = a.n;
+    Matrix<MOD> c(n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < n; k++)
                 c[i][j] = (c[i][j] + 1LL * a[i][k] * b[k][j]) % MOD;
     return c;
 }
 
-template<int N, int MOD>
-Matrix<N, MOD> pwr(Matrix<N, MOD> a, int n) {
+template<int MOD>
+Matrix<MOD> pwr(Matrix<MOD> a, int n) {
     if (!n)
-        return Matrix<N, MOD>::null();
+        return Matrix<MOD>::null(a.n);
     if (n % 2)
         return a * pwr(a * a, n / 2);
     return pwr(a * a, n / 2);
